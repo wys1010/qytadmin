470c2ce553019d987e7217f5e78be4e0812be80e
Xandy <1010wqs@163.com>
2015-10-19 16:02:01 +0800

    优化

--- dbscript/modify.sql
+++ dbscript/modify.sql
@@ -1,3 +1,3 @@
 alter table warehouses add  column attendant_id int(11) COMMENT '管理员,员工id' AFTER attendant;
 alter table warehouses add  constraint fk_attendant_staff foreign key(attendant_id) REFERENCES staffs(id);
-
+alter table warehouses DROP COLUMN attendant;
